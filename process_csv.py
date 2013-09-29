4c4a189f0904ec716e2dfa3408e68a423f218ef9
I K Stead <iste043@aucklanduni.ac.nz>
2013-09-30 00:09:55 +1300

    Initial commit

---
+++ process_csv.py
@@ -0,0 +1,96 @@
+"""
+Solution for automatically generating EOD reports from List of Sessions
+csv exports
+"""
+__author__ = 'Isaac'
+
+import csv
+import re
+from datetime import timedelta
+
+EXAMPLE = 'C:/Users/Isaac/Projects/python-list-of-sessions/los_export.csv'
+LEN_REGEX = re.compile("\d+:\d+:\d+")
+
+
+def sum_durations(records):
+    """Given a dictionary of records, find and sum their durations"""
+    durations = [record['duration'] for record in records]
+    total = reduce(sum, durations, timedelta(0))
+
+    return total
+
+def convert_types(rows):
+    """Attempt to convert data of various types"""
+    out = []
+
+    for row in rows:
+        new = []
+        for string in row:
+            # Convert durations, in the format hh:mm:ss
+            if re.match("^\d+:\d+:\d+$", string):
+                h, m, s = map(int, string.split(':'))
+                duration = timedelta(hours=h, minutes=m, seconds=s)
+                new.append(duration)
+            # Convert integers
+            elif re.match("^\d+$", string):
+                new.append(int(string))
+            # Convert "yes" or "no" to True or False
+            elif re.match("^yes$", string, re.IGNORECASE):
+                new.append(True)
+            elif re.match("^no$", string, re.IGNORECASE):
+                new.append(False)
+            # No matches for conversion
+            else:
+                new.append(string)
+        out.append(new)
+
+    return out
+
+
+def convert_duration_str(string):
+    """Convert a string in the format hh:mm:ss to timedelta"""
+    if re.search("\d+:\d+:\d+", string):
+        h, m, s = string.split(':')
+        duration = timedelta(hours=h, minutes=m, seconds=s)
+    else:
+        raise Exception("Incorrect format for duration string")
+
+    return duration
+
+
+def convert_csv(rows):
+    """Convert array of csv rows into a dictionary"""
+    headers = map(lambda h: h.lower().replace(' ', '_'), rows[0])
+    out = []
+
+    for row in rows[1:]:
+        row_dict = {h: v for h, v in zip(headers, row)}
+        out.append(row_dict)
+
+    return out
+
+
+def fix_rows(rows):
+    """Fix rows malformed due to commas in recording name field"""
+    # TODO: Magic numbers
+    out = []
+
+    for row in rows:
+        if len(row) > 12:
+            offset = len(row) - 7
+            rec_name = ''.join(row[4:offset])
+            fixed = row[:4] + [rec_name] + row[offset:]
+            out.append(fixed)
+        else:
+            out.append(row)
+
+    return out
+
+
+def load_csv(filename):
+    """Slurp all rows in given filename into list"""
+    out = []
+    with open(filename) as csv_file:
+        out = [row for row in csv.reader(csv_file)]
+
+    return out
