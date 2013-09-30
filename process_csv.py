e48538dee65c1140bf71cac8f5f4526444afcbbf
I K Stead <iste043@aucklanduni.ac.nz>
2013-10-01 00:27:41 +1300

    Added functions to split records by unique value and load and process file

--- process_csv.py
+++ process_csv.py
@@ -7,20 +7,76 @@ __author__ = 'Isaac'
 import csv
 import re
 from datetime import timedelta
+from collections import namedtuple
 
 EXAMPLE = 'C:/Users/Isaac/Projects/python-list-of-sessions/los_export.csv'
 
-
-class ModDict(dict):
+class Records(dict):
     """Subclass of dict to allow setting extra attributes"""
     pass
 
 
+def generate_eod_report(records):
+    """Generate end of day report from list of sessions csv export"""
+    segments = split_records_by_segment(records)
+    stages_per_seg = {k: split_records_by_stage(v) for k, v in segments.iteritems()}
+    report_str = """
+    {}
+
+    - Transcribing - {}
+    - Ready for QA - {}
+    - QA in progress - {}
+    - Ready for review - {}
+    - Review in progress {}
+    """
+    for segment in stages_per_seg:
+        pass
+
+
+def split_records_by_segment(records):
+
+    records_by_segment = {'b2b': [], 'b2c': [], 'spanish': [], 'srt': []}
+    for record in records:
+        customer = record['customer']
+        if '@transcribeme.com' in customer:
+            if 'srt' in customer:
+                records_by_segment['srt'].append(record)
+            elif 'spanish' in customer:
+                records_by_segment['spanish'].append(record)
+            else:
+                records_by_segment['b2b'].append(record)
+        else:
+            records_by_segment['b2c'].append(record)
+
+    return records_by_segment
+
+
+def split_records_by_stage(records):
+    """Divide records according to what stage of processing they're at"""
+    records_by_stage = {'transcribing': [], 'ready_for_qa': [],
+                        'qa_in_progress': [], 'ready_for_review': [],
+                        'review_in_progress': [], 'completed': []}
+    for record in records:
+        if not record['is_finished?']:
+            records_by_stage['transcribing'].append(record)
+        elif record['is_finished?'] and not record['qa']:
+            records_by_stage['ready_for_qa'].append(record)
+        elif 'Expires' in record['qa']:
+            records_by_stage['qa_in_progress'].append(record)
+        elif 'Submitted' in record['qa'] and not record['review']:
+            records_by_stage['ready_for_review'].append(record)
+        elif 'Expires' in record['review']:
+            records_by_stage['review_in_progress'].append(record)
+        else:
+            records_by_stage['completed'].append(record)
+
+    return records_by_stage
+
+
 def split_by_unique_value(field, records):
     """Split a list of records into sublists by common field"""
-    out = ModDict()
+    out = Records()
     out.field = field # Allows checking which field was split on
-
     for record in records:
         value = record[field]
         if value not in out.keys():
@@ -82,7 +138,6 @@ def convert_csv(rows):
     """Convert list of csv rows into a dictionary"""
     headers = map(lambda h: h.lower().replace(' ', '_'), rows[0])
     out = []
-
     for row in rows[1:]:
         row_dict = {h: v for h, v in zip(headers, row)}
         out.append(row_dict)
@@ -94,7 +149,6 @@ def fix_rows(rows):
     """Fix rows malformed due to commas in recording name field"""
     # TODO: Magic numbers
     out = []
-
     for row in rows:
         if len(row) > 12:
             offset = len(row) - 7
@@ -114,3 +168,15 @@ def load_csv(filename):
         out = [row for row in csv.reader(csv_file)]
 
     return out
+
+
+def partition(predicate, iterable):
+    match = []
+    no_match = []
+    for item in iterable:
+        if predicate(item):
+            match.append(item)
+        else:
+            no_match.append(item)
+
+    return match, no_match
